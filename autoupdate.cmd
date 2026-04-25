4ec700f3bc90e0eeaaaa817022dcc51cde84c206
SirNote <siroshy@ya.ru>
2026-04-25 13:31:32 +0300

    Удаление автообновления из репозитория

--- autoupdate.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001
-echo Обновление репозитория %TIME% %DATE% >> autoupdateLog.log 2>&1
-git pull origin main >> autoupdateLog.log 2>&1
-echo ---------------------------------------------------------------- >> autoupdateLog.log 2>&1
-echo Обновление завершено. Смотри autoupdateLog.log
-echo ----------------------------------------------------------------
