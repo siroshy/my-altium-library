f0f2291a44ba7094729123eef8a8d5b20de85367
SirNote <siroshy@ya.ru>
2026-04-25 13:30:31 +0300

    небольшая правка автообновления

--- autoupdate.cmd
+++ autoupdate.cmd
@@ -3,3 +3,5 @@ chcp 65001
 echo Обновление репозитория %TIME% %DATE% >> autoupdateLog.log 2>&1
 git pull origin main >> autoupdateLog.log 2>&1
 echo ---------------------------------------------------------------- >> autoupdateLog.log 2>&1
+echo Обновление завершено. Смотри autoupdateLog.log
+echo ----------------------------------------------------------------
